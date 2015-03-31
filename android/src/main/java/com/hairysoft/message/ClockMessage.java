d0ca5bd8f5038763f29302e6b568db5ff18adc50
crazyfacka <gmail@joagonca.com>
2015-03-31 22:12:33 +0100

    Now takes into account daylight saving time

--- android/src/main/java/com/hairysoft/message/ClockMessage.java
+++ android/src/main/java/com/hairysoft/message/ClockMessage.java
@@ -1,12 +1,19 @@
 package com.hairysoft.message;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  * Message to sync the clock time
  */
 public class ClockMessage extends BaseMessage {
     public long timestamp;
     public ClockMessage() {
-        this.timestamp = System.currentTimeMillis() / 1000L;
+        Calendar c = Calendar.getInstance();
+        if(TimeZone.getDefault().inDaylightTime(c.getTime())) {
+            c.add(Calendar.HOUR, 1);
+        }
+        this.timestamp = new Long(c.getTime().getTime() / 1000);
     }
 
     public ClockMessage(long timestamp) {
